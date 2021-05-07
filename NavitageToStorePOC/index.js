8c5b73fb944e368fa2af243d1a2edc1c059ff4dc
Jorge Soriano <george.soriano.plus@gmail.com>
2021-05-07 11:18:49 -0500

    toStore2 test

--- NavitageToStorePOC/index.js
+++ NavitageToStorePOC/index.js
@@ -1,14 +1,23 @@
+//https://app.urlgeni.us/?
+//https://urlgeni.us/IMSSDigital
 function navigateToStore(){
 
     const userAgent = navigator.userAgent;
+    var store = "";
 
     if((/Mobi|Android/i.test(userAgent))){
-        window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico';
+        //window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico';
+        store = PlayStore;
     }
     if(/iPad|iPhone|iPod/i.test(userAgent)){
-        window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8';
+        //window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8';
+        store = AppStore;
     }
     if(/windows/i.test(userAgent)){
         alert("Windows PC");
     }
+}
+
+function toStore2(){
+    window.location.href = 'https://urlgeni.us/IMSSDigital';
 }
