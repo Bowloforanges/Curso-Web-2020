7c3e197aac39a94393945c27d96cfa2b0ab577f6
Jorge Soriano <george.soriano.plus@gmail.com>
2021-05-07 14:24:02 -0500

    test 7 POC

--- NavitageToStorePOC/index.js
+++ NavitageToStorePOC/index.js
@@ -5,13 +5,16 @@ function navigateToStore(){
     const userAgent = navigator.userAgent;
 
     if((/Mobi|Android/i.test(userAgent))){
-        //window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico';
-        window.location.href = 'st.android.imsspublico://IMSSDigital';
+        try{
+            window.location.href = 'st.android.imsspublico://IMSSDigital';
+        } catch (exception){
+            console.log(exception);
+            window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico';
+        }
     } else if(/iPad|iPhone|iPod/i.test(userAgent)){
-        gob.mx.imss.digital
+        //gob.mx.imss.digital
         //window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8';
-        //window.location.href = 'gob.mx.imss.digital://IMSSDigital';
-        window.location.href = 'imss-digital://IMSSDigital';
+        window.location.href = 'gob.mx.imss.digital://IMSSDigital';
     } else {
         alert("Por favor abra esta página en un dispositivo móvil para descargar la aplicación IMSS Digital.");
     }
