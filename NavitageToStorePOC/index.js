dd0ca6328a7fb04c33c15ec5ed75058e439ccc95
Jorge Soriano <george.soriano.plus@gmail.com>
2021-05-07 14:49:36 -0500

    test 7.3 POC

--- NavitageToStorePOC/index.js
+++ NavitageToStorePOC/index.js
@@ -6,18 +6,27 @@ function navigateToStore(){
 
     if((/Mobi|Android/i.test(userAgent))){
 
-        window.location.href = 'st.android.imsspublico://IMSSDigital';
+        window.location.href = 'st.android.imsspublico://IMSSDigital'; //deeplink
 
         setTimeout(function(){ 
 
-            window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico';
-            
+            window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico'; //playstore link
+
         }, 2000);
     
     } else if(/iPad|iPhone|iPod/i.test(userAgent)){
-        //gob.mx.imss.digital
         //window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8';
-        window.location.href = 'gob.mx.imss.digital://IMSSDigital';
+        //window.location.href = 'gob.mx.imss.digital://IMSSDigital';
+
+        window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8'; //deeplink (inexistente)
+
+        setTimeout(function(){ 
+
+            window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8'; //appstore link
+            
+        }, 2000);
+
+
     } else {
         alert("Por favor abra esta página en un dispositivo móvil para descargar la aplicación IMSS Digital.");
     }
