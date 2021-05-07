82cf3729c4f913dd89a01a818c6edb17c8d3172c
Jorge Soriano <george.soriano.plus@gmail.com>
2021-05-07 13:16:01 -0500

    test 4 POC

--- NavitageToStorePOC/index.js
+++ NavitageToStorePOC/index.js
@@ -5,13 +5,13 @@ function navigateToStore(){
     const userAgent = navigator.userAgent;
 
     if((/Mobi|Android/i.test(userAgent))){
-        window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico';
-    }
-    if(/iPad|iPhone|iPod/i.test(userAgent)){
+        //window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico';
+        window.location.href = 'st.android.imsspublico://IMSSDigital/Home';
+    } else if(/iPad|iPhone|iPod/i.test(userAgent)){
         window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8';
+    } else {
+        alert("Por favor abra esta página en un dispositivo móvil para descargar la aplicación IMSS Digital.");
     }
 
-    alert("Por favor abra esta página en un dispositivo móvil para descargar la aplicación IMSS Digital.");
-    
 }
 
