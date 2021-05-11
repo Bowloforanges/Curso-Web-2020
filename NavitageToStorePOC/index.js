11b0850329b2459cc011d7af6a4d0e46c058d57e
Jorge Soriano <george.soriano.plus@gmail.com>
2021-05-11 14:31:31 -0500

    poc v7.7

--- NavitageToStorePOC/index.js
+++ NavitageToStorePOC/index.js
@@ -3,33 +3,34 @@
 function navigateToStore(){
 
     const userAgent = navigator.userAgent;
+    var device = "";
 
     if((/Mobi|Android/i.test(userAgent))){
-
-        window.location.href = 'st.android.imsspublico://IMSSDigital'; //deeplink
-
-        setTimeout(function(){ 
-
-            window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico'; //playstore link
-
-        }, 2000);
-    
-    } else if(/iPad|iPhone|iPod/i.test(userAgent)){
-        //window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8';
-        //window.location.href = 'gob.mx.imss.digital://IMSSDigital';
-
-        window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8'; //deeplink (inexistente)
-
-        setTimeout(function(){ 
-
-            window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8'; //appstore link
-            
-        }, 2000);
-
-
-    } else {
-        alert("Por favor abra esta página en un dispositivo móvil para descargar la aplicación IMSS Digital.");
+        device = "android";
+    } 
+    if(/iPad|iPhone|iPod/i.test(userAgent)){
+        device = "ios";
     }
-    
+
+
+    switch(device){
+
+        case 'android':
+            window.location.href = 'st.android.imsspublico://IMSSDigital'; //deeplink
+            setTimeout(function(){ 
+                window.location.href = 'https://play.google.com/store/apps/details?id=st.android.imsspublico'; //playstore link
+            }, 2000);
+            break;
+
+        case 'ios':
+            window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8'; //deeplink (inexistente)
+            break;
+
+        default:
+
+            alert("Por favor abra esta página en un dispositivo móvil para descargar la aplicación IMSS Digital.");
+
+    }
+        
 }
 
