9671cd0aa1096f11911affd906e63930db28a203
Jorge Soriano <george.soriano.plus@gmail.com>
2021-05-12 16:57:19 -0500

    poc Final 5

--- NavitageToStorePOC/index.js
+++ NavitageToStorePOC/index.js
@@ -4,15 +4,9 @@ function navigateToStore() {
 
     if ((/Mobi|Android/i.test(userAgent))) {
         device = "android";
-        if (document.readyState === 'complete') {
-            window.close();
-        }
     }
     if (/iPad|iPhone|iPod/i.test(userAgent)) {
         device = "ios";
-        if (document.readyState === 'complete') {
-            window.close();
-        }
     }
 
     switch (device) {
@@ -24,12 +18,15 @@ function navigateToStore() {
             }, 2000);
             break;
         case 'ios':
-            window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8'; //deeplink (inexistente)
+            window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8';
+            setTimeout(function () {
+                window.close();
+            }, 2000);
             break;
         default:
             alert("Por favor abra esta página en un dispositivo móvil para descargar la aplicación IMSS Digital.");
-            if (document.readyState === 'complete') {
+            setTimeout(function () {
                 window.close();
-            }
+            }, 2000);
     }
 }
