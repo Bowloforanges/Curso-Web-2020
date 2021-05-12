b827e62126cc988d353d2feae17c148c2e4c5d33
armandpaez <apaez77@gmail.com>
2021-05-12 18:58:04 -0500

    Update index.js
    Cambio en index

--- NavitageToStorePOC/index.js
+++ NavitageToStorePOC/index.js
@@ -20,10 +20,10 @@ function navigateToStore() {
             }, 2000);
             break;
         case 'ios':
-            window.location.href = 'IMSS_Digital://'; //deeplink
-            setTimeout(() => {
-                window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8';
-            }, 10000);
+            window.location.replace("IMSS Digital://"); //deeplink
+            setTimeout(function () {
+                window.location.href = 'https://itunes.apple.com/us/app/imss-digital/id975273006?mt=8'; //appstore link
+            }, 2000);
             break;
         default:
             alert("Por favor abra esta página en un dispositivo móvil para descargar la aplicación IMSS Digital.");
